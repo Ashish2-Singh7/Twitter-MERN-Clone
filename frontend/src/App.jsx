3284bfa5ab3f77f816fee2bdf42932e8d03d62c5
Ashish <ashishsingh272007@gmail.com>
2025-03-14 20:44:26 +0530

    3- Auth handled and routes protected in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/auth/Login/LoginPage.jsx";
 import SignUpPage from "./pages/auth/Signup/SignUpPage.jsx";
@@ -9,21 +9,55 @@ import ProfilePage from "./pages/profile/ProfilePage.jsx";
 import Sidebar from './components/common/Sidebar.jsx';
 import RightPanel from "./components/common/RightPanel.jsx";
 
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner.jsx";
+
 
 function App() {
+
+  const { data: authUser, isLoading } = useQuery({
+    // we use queryKey to give a unique name to our query and refer to it later
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (data.error) return null;
+        if (!res.ok) { // or can write if(data.error)
+          throw new Error(data.error || "Something went wrong");
+        }
+        console.log("authUser is here: ", data);
+        return data;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+    retry: false, // only retry's once and reduces the loading time which we were getting when we were not login and reloading the page
+  });
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
 
   return (
     <div className="flex max-w-6xl mx-auto">
       {/* Common component, bc it's not wrapped with Routes */}
-      <Sidebar />
+      {authUser && <Sidebar />}
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
-        <Route exact path='/login' element={<LoginPage />} />
-        <Route exact path='/signup' element={<SignUpPage />} />
-        <Route exact path='/notifications' element={<NotificationPage />} />
-        <Route exact path='/profile/:username' element={<ProfilePage />} />
+        <Route exact path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route exact path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route exact path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route exact path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
+        <Route exact path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
-      <RightPanel />
+      {authUser && <RightPanel />}
+      <Toaster />
     </div>
   )
 }
