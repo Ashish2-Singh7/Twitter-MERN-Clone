9cb03a61900fc976b0f1ddc60e153960b4ef4007
Ashish <ashishsingh272007@gmail.com>
2025-03-14 16:53:10 +0530

    2- UI Design Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+
+import LoginPage from "./pages/auth/Login/LoginPage.jsx";
+import SignUpPage from "./pages/auth/Signup/SignUpPage.jsx";
+import HomePage from "./pages/home/HomePage.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
+
+import Sidebar from './components/common/Sidebar.jsx';
+import RightPanel from "./components/common/RightPanel.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      {/* Common component, bc it's not wrapped with Routes */}
+      <Sidebar />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/login' element={<LoginPage />} />
+        <Route exact path='/signup' element={<SignUpPage />} />
+        <Route exact path='/notifications' element={<NotificationPage />} />
+        <Route exact path='/profile/:username' element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+    </div>
   )
 }
 
